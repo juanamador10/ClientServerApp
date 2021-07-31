6bde468589454d8346d3746c9aedf633a496ed39
Juan Amador <juan.amador.is@unipolidgo.edu.mx>
2021-07-31 15:55:19 -0500

    Third Commit

--- src/app/shared/ribbon/ribbon.component.ts
+++ src/app/shared/ribbon/ribbon.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-ribbon',
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ribbon.component.css']
 })
 export class RibbonComponent implements OnInit {
-
+  @Input() folder:string='';
+  @Input() title: string='';
   constructor() { }
 
   ngOnInit(): void {
